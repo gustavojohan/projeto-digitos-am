521762256d6e9430580807d31f43c89fd28654bd
Gustavo <gustavojohan200@gmail.com>
2026-02-26 04:14:17 -0300

    Finaliza o projeto

--- regressao_logistica_wd.py
+++ regressao_logistica_wd.py
@@ -1,28 +1,24 @@
 import numpy as np
 
-class LogisticRegression_:
-    def __init__(self, eta=0.1, tmax=1000, bs=1000000):
-      self.eta = eta
-      self.tmax = tmax
-      self.batch_size = bs
+class LogisticRegressionWD_:
+    def __init__(self, eta=0.1, tmax=1000, lamb=0.01):
+        self.eta = eta
+        self.tmax = tmax
+        self.lamb = lamb # Parâmetro de regularização (lambda)
 
-    # Infere o vetor w da funçao hipotese
-    #Executa a minimizao do erro de entropia cruzada pelo algoritmo gradiente de descida
     def fit(self, _X, _y):
         X = np.array(_X)
         y = np.array(_y)
-
-        N = X.shape[0]
-        d = X.shape[1]
-
+        N, d = X.shape
         self.w = np.zeros(d)
 
         for t in range(self.tmax):
             wx = X.dot(self.w)
-            ywx = y * wx
-            v = y / (1 + np.exp(ywx))
-            gt = -(1/N) * X.T.dot(v)
-
+            v = y / (1 + np.exp(y * wx))
+            
+            # Gradiente original + termo de regularização (Weight Decay)
+            gt = -(1/N) * X.T.dot(v) + (2 * self.lamb / N) * self.w
+            
             self.w = self.w - self.eta * gt
     
         
